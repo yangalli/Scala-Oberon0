0d4fba5d11fbebb5100f91ada503bf2bc845f6f3
Yan Galli <yangalli@cjr.org.br>
2018-05-18 15:41:25 -0300

    doing some fixes

--- src/main/scala/oberon/expression/BinExpression.scala
+++ src/main/scala/oberon/expression/BinExpression.scala
@@ -8,34 +8,113 @@ class AddExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs,
 
   override
   def eval() : Value = {
-    val v1 : IntValue = lhs.eval().asInstanceOf[IntValue]
-    val v2 : IntValue = rhs.eval().asInstanceOf[IntValue]
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
 
-    return new IntValue(v1.value + v2.value)
+    return IntValue(v1 + v2)
   }
 }
 
-class EqExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+class MultExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
+
+    return IntValue(v1 * v2)
+  }
+}
+
+class LeExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
 
   override
   def eval: Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
+
+    return BoolValue(v1 <= v2) 
+  }
+
+}
+class EqExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
     val v1 = lhs.eval()
     val v2 = rhs.eval()
 
-    val res = v1 == v2
-    return BoolValue(res) 
+    return BoolValue(v1 == v2) 
   }
 
+}
+
+class GtExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
+
+    return BoolValue(v1 > v2) 
+  }
+
+}
+
+class LtExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
+
+    return BoolValue(v1 < v2) 
+  }
 }
 
 class SubtractExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
 
   override
   def eval() : Value = {
-    val v1 : IntValue = lhs.eval().asInstanceOf[IntValue]
-    val v2 : IntValue = rhs.eval().asInstanceOf[IntValue]
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
 
-    return new IntValue(v1.value - v2.value)
+    return new IntValue(v1 - v2)
+  }
+
+}
+
+class AndExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
+    val v1 = lhs.eval().asInstanceOf[BoolValue].value
+    val v2 = rhs.eval().asInstanceOf[BoolValue].value
+
+    return BoolValue(v1 && v2) 
+  }
+
+}
+
+class NotExpression(expression: Expression) extends Expression {
+
+  override
+  def eval() : Value = {
+    val val1 = expression.eval().asInstanceOf[BoolValue].value
+    
+    return BoolValue(!val1) 
+  }
+
+}
+
+class NotEqExpression(lhs: Expression, rhs: Expression) extends BinExpression(lhs, rhs) {
+
+  override
+  def eval() : Value = {
+    val v1 = lhs.eval()
+    val v2 = rhs.eval()
+
+    return new NotExpression(new EqExpression(v1, v2)).eval()
   }
 
 }
@@ -44,10 +123,10 @@ class DivideExpression(lhs: Expression, rhs: Expression) extends BinExpression(l
 
   override
   def eval() : Value = {
-    val v1 : IntValue = lhs.eval().asInstanceOf[IntValue]
-    val v2 : IntValue = rhs.eval().asInstanceOf[IntValue]
+    val v1 = lhs.eval().asInstanceOf[IntValue].value
+    val v2 = rhs.eval().asInstanceOf[IntValue].value
 
-    return new IntValue(v1.value / v2.value)
+    return new IntValue(v1 / v2)
   }
 
 }
